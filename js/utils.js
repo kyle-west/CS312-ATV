aa174abc0b4154a819a7f9a9f1256dabf6a78aaf
Kyle West <Kyle West>
2017-04-03 17:45:21 -0600

    Annotated Utils file

--- js/utils.js
+++ js/utils.js
@@ -1,12 +1,12 @@
-/**********************************************************
-* Random Integer function returns int between min and max
-* @param pos_neg, returns a random positive or negative value
-**********************************************************/
+/****************************************************************
+* Random Integer function returns int between min and max values
+* @param pos_neg, allows negative inversal of the random int.
+****************************************************************/
 function random(min, max, pos_neg = false) {
    var d = 1;// direction
    var val = Math.floor(Math.random() * max);
    if (pos_neg) {
       d = Math.pow(-1, val);
    }
-   return d*val;
+   return d * val;
 }
