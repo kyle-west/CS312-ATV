9de01ef949db2594cbf9f5a657a056a617594dee
Kyle West <Kyle West>
2017-04-05 15:09:18 -0600

    Took screenshots

--- js/settings.js
+++ js/settings.js
@@ -25,5 +25,5 @@ const OPTIONS = {
 **************************************************/
 var SETTINGS = {
    sound : OPTIONS.sound.ON,
-   game  : OPTIONS.game.PLAY
+   game  : OPTIONS.game.DEV
 };
