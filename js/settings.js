50cf075d077dd7709422c63148de3892b4dae7dd
Kyle West <kyle-west@users.noreply.github.com>
2017-04-06 21:33:15 -0600

    Put in PLAY mode from DEV mode

--- js/settings.js
+++ js/settings.js
@@ -25,5 +25,5 @@ const OPTIONS = {
 **************************************************/
 var SETTINGS = {
    sound : OPTIONS.sound.ON,
-   game  : OPTIONS.game.DEV
+   game  : OPTIONS.game.PLAY
 };
